001bf98cb20c4782795cae4fbf9f75f300c1218d
Hans Fjallemark <hans.fjallemark@cignium.com>
2017-08-12 07:40:08 +0200

    Add source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const merge = require('webpack-merge')
-const webpack = require('webpack')
-
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
+const merge = require("webpack-merge")
+const webpack = require("webpack")
 const common = {
+  devtool: "source-map",
   module: {
     rules: [
       {
         include: /example|src/,
-        loader: 'awesome-typescript-loader',
+        loader: "awesome-typescript-loader",
         test: /\.tsx?$/,
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
+    extensions: [".js", ".ts", ".tsx"],
   },
 }
 
-if (process.env.npm_lifecycle_event === 'build') {
+if (process.env.npm_lifecycle_event === "build") {
   module.exports = merge(common, {
-    entry: './src',
+    entry: "./src",
     output: {
-      filename: 'jelms.js',
-      path: __dirname + '/dist',
+      filename: "jelms.js",
+      path: __dirname + "/dist",
+      sourceMapFilename: "jelms.js.map",
     },
-    plugins: [new UglifyJSPlugin()],
+    plugins: [new UglifyJSPlugin({ sourceMap: true })],
   })
 } else {
   module.exports = merge(common, {
     devServer: {
-      contentBase: 'example',
+      contentBase: "example",
       hot: true,
     },
-    devtool: 'source-map',
-    entry: './example',
+    entry: "./example",
     output: {
-      filename: 'app.js',
+      filename: "app.js",
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
